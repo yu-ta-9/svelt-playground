f2bdfb258c1439b1b28809f1191cef00347d75f7
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-10-12 09:37:54 +0900

    add button component

---
+++ src/components/ui/button/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+export let label:string;
+</script>
+
+<button>{label}</button>
+
+<style>
+  button {
+    background-color: red;
+  }
+</style>
---
+++ src/routes/graph/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import Button from '../../components/ui/button/index.svelte';
+</script>
+
+<svelte:head>
+  <title>Graph</title>
+  <meta name="description" content="Graph" />
+</svelte:head>
+
+<div>
+  <h1>Graph</h1>
+
+  <Button label="Show!!" />
+</div>
